634170dccd872fbcbe21eb56cd6deea296e430f7
Kamrulthedev <kamrulthedevR@gmail.com>
2025-08-21 14:55:33 +0600

    title-sutomaize

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,7 +10,7 @@ import ResponsiveNav from "./Nav/ResponsiveNav";
 const Header = () => {
   return (
     <header className="bg-white border-b">
-      <div className="container mx-auto flex items-center justify-between md:py-6 ">
+      <div className="container mx-auto flex items-center justify-between md:py-4">
         <ResponsiveNav />
         <Link className="cursor-pointer flex items-center lg:p-2 p-4 gap-2 bg-white rounded-2xl
                    transition-all duration-300 ease-in-out hover:scale-105"
@@ -23,11 +23,11 @@ const Header = () => {
           <img
             src={logo}
             alt="Trip Seeker Logo"
-            className="w-12 h-12 rounded-full object-cover ring-2 ring-indigo-300 ring-offset-2 shadow-md
-                     transition-transform duration-300 ease-in-out hover:rotate-6"
+            className="w-12 h-12 rounded-full object-cover ring-2 bg-gradient-to-r from-blue-600 to-green-500 ring-offset-2 shadow-md
+                     transition-transform duration-300 ease-in-out hover:rotate-12"
           />
           <h1 className="text-4xl font-extrabold tracking-wider
-                       text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-indigo-700
+                       text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-green-500
                        drop-shadow-lg uppercase
                        transition-colors duration-300">
             TRIP SEEKER
