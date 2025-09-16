06611901b85c42197b117955cca9d5f50dfe027f
Kamrulthedev <kamrulthedevR@gmail.com>
2025-09-16 23:08:47 +0600

    update-Cart-Sheet

--- src/components/cart/CartSheet.tsx
+++ src/components/cart/CartSheet.tsx
@@ -45,7 +45,7 @@ export const CartSheet = ({ isOpen, onClose }: { isOpen: boolean, onClose: () =>
                         initial={{ x: "100%" }}
                         animate={{ x: 0 }}
                         exit={{ x: "100%" }}
-                        transition={{ type: "spring", stiffness: 400, damping: 40 }}
+                        transition={{ type: "spring", stiffness: 300, damping: 30 }}
                         onClick={(e) => e.stopPropagation()}
                     >
                         {/* Header */}
